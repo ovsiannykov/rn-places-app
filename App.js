7da52bb98edc066b7306632f5001b32b0b2dc720
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-03-14 18:39:46 +0200

    244. Diving into SQLite for Permanent Data Storage

--- App.js
+++ App.js
@@ -6,6 +6,16 @@ import ReduxThunk from "redux-thunk";
 
 import placesReducer from "./store/places-reducer";
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import { init } from "./helpers/db";
+
+init()
+  .then(() => {
+    console.log("Initialized database 🎉");
+  })
+  .catch((err) => {
+    console.log("Initializing db failed 😢");
+    console.log(err);
+  });
 
 const rootReducer = combineReducers({
   places: placesReducer,
