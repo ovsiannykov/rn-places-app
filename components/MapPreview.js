09435aa275562db4fb3d1ccd6d77ac488ec4fb22
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-03-15 14:48:01 +0200

    Refactoring Maps Key

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, Image, StyleSheet, Text } from "react-native";
 
+import ENV from "../env";
+
 const MapPreview = (props) => {
   let imagePreviewUrl;
 
   if (props.location) {
     const loc = props.location;
 
-    imagePreviewUrl = `https://api.mapbox.com/styles/v1/mapbox/light-v10/static/pin-l+ff0f0f(${loc.longitude},${loc.latitude})/${loc.longitude},${loc.latitude},16,0/400x200?before_layer=bridge-major-link-2&access_token=pk.eyJ1Ijoib3ZzaWFubnlrb3Y5OCIsImEiOiJjbDBzMXBteXUwMzYzM2lwZ2J1czdwa3E5In0.noSP3568NE_NXCQ8vYTY7Q`;
+    imagePreviewUrl = `https://api.mapbox.com/styles/v1/mapbox/light-v10/static/pin-l+ff0f0f(${loc.longitude},${loc.latitude})/${loc.longitude},${loc.latitude},16,0/400x200?before_layer=bridge-major-link-2&access_token=${ENV.mapsApiKey}`;
   }
 
   return (
---
+++ env.js
@@ -0,0 +1,6 @@
+const vars = {
+  mapsApiKey:
+    "pk.eyJ1Ijoib3ZzaWFubnlrb3Y5OCIsImEiOiJjbDBzMXBteXUwMzYzM2lwZ2J1czdwa3E5In0.noSP3568NE_NXCQ8vYTY7Q",
+};
+
+export default vars;
